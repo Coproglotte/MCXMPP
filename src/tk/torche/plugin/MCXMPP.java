76f731531e7efe716389f6eb747aeb77325ff1d5
Coproglotte <coproglotte@openmailbox.org>
2015-08-06 00:17:12 +0200

    XMPP connection when enabling plugin

--- src/tk/torche/plugin/MCXMPP.java
+++ src/tk/torche/plugin/MCXMPP.java
@@ -3,13 +3,23 @@ package tk.torche.plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import tk.torche.plugin.util.Config;
+import tk.torche.plugin.util.XMPPHandler;
 
 
 public class MCXMPP extends JavaPlugin {
 
+	private Config config;
+	private XMPPHandler XMPPh;
+
 	@Override
 	public void onEnable() {
-		Config config = new Config(this.getConfig());
-		System.out.println("[MCXMPP v1.0] Plugin loaded.");
+		config = new Config(this.getConfig());
+		XMPPh = new XMPPHandler(this);
+		XMPPh.connect();
+		System.out.println("[MCXMPP v0.1] Plugin loaded.");
+	}
+
+	public Config getLoadedConfig() {
+		return this.config;
 	}
 }
---
+++ src/tk/torche/plugin/util/XMPPHandler.java
@@ -0,0 +1,34 @@
+package tk.torche.plugin.util;
+
+import org.jivesoftware.smack.AbstractXMPPConnection;
+import org.jivesoftware.smack.tcp.XMPPTCPConnection;
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+
+import tk.torche.plugin.MCXMPP;
+
+
+public class XMPPHandler {
+
+	private XMPPTCPConnectionConfiguration connConf;
+	private AbstractXMPPConnection conn;
+
+	public XMPPHandler(MCXMPP instance) {
+		setXMPPConnectionConfiguration(instance.getLoadedConfig());
+	}
+
+	private void setXMPPConnectionConfiguration(Config config) {
+		connConf = XMPPTCPConnectionConfiguration.builder()
+				.setUsernameAndPassword(config.getUsername(), config.getPassword())
+				.setHost(config.getHost())
+				.setPort(config.getPort())
+				.build();
+	}
+
+	public void connect() {
+		conn = new XMPPTCPConnection(connConf);
+	}
+
+	public AbstractXMPPConnection getXMPPConnection() {
+		return conn;
+	}
+}
